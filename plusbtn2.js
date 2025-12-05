27aebdd7dd0c453c01415166d87405d0165a9591
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-05 14:17:26 +0530

    files

--- plusbtn2.js
+++ plusbtn2.js
@@ -1,16 +1,28 @@
-  const button = document.getElementById('menu-button');
-    const dropdown = document.getElementById('dropdown');
-    const input=document.getElementById('files');
-    const plus=document.getElementById('plus');
-    button.addEventListener('click', () => {
-      dropdown.classList.toggle('hidden');
-    });
-    // Close dropdown if clicked outside
-     window.addEventListener('click', function(e) {
-      if (!button.contains(e.target) && !dropdown.contains(e.target)) {
-        dropdown.classList.add('hidden');
-      }
-    });
-  plus.addEventListener('click',()=>{
-    input.classList.toggle('hidden');
-  });
+const button = document.getElementById('menu-button');
+const dropdown = document.getElementById('dropdown');
+const plus = document.getElementById('plus');
+const files = document.getElementById('files');
+
+button.addEventListener('click', () => {
+  dropdown.classList.toggle('hidden');
+});
+
+// Close dropdown when clicked outside
+window.addEventListener('click', (e) => {
+  if (!button.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.classList.add('hidden');
+  }
+});
+
+// Add new file input on + click
+plus.addEventListener('click', () => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.name = "file-" + Date.now();
+
+  input.className = "inline-block mt-3 bg-white text-black p-2 rounded-md mr-5 mr-l-2";
+
+  files.appendChild(input);
+});
+
+
