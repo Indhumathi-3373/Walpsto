b1fe9db0d3bd33a457e987e27a7c77552ac46c2e
indhumathi-3373 <indhumathi93428@gmail.com>
2025-12-13 11:46:09 +0530

    backend

--- frontend/login_for_diary.js
+++ frontend/login_for_diary.js
@@ -6,28 +6,36 @@ const errormsg2=document.getElementById("error1")
 const forgot=document.getElementById("f_btn")
 const create=document.getElementsByClassName("c_btn")
 const emailRegex=/^[^\\s@]+@[^\\s@]+\\. [^\\s@]+$/;
-form.addEventListener("submit",(e)=>{
-    var email=emailinp.value.trim()
-    var password=passinp.value.trim()
+form.addEventListener("submit",async(e)=>{
+    const email=emailinp.value.trim()
+    const pass_diary=passinp.value.trim()
     if(emailinp.value==="" || emailRegex.test(email)){
          errormsg.style.display="block"
     }else{
         errormsg.style.display="none"
     }
-    if(password.length <= 6){
+    if(pass_diary.length <= 6){
         errormsg2.style.display="block" 
     }else{
         errormsg2.style.display="none"
-    }
-    if(password.length > 6 && emailinp.value !="" ){
-        window.open("diary.html")
     }
     e.preventDefault();
 
     //sending details to backend
 
-    const 
-
+    const res=await fetch("http://localhost:8000/frontend/loginfordiary",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify({email,pass_diary})
+    })
+    const response=await res.json();
+    if(response.message==="Login Successful"){
+    alert("Login successful")}
+    if(response.message==="Email not registered please create an account"){
+        alert("Email not registered please create an account")
+    }if(response.message==="Incorrect Password"){
+        alert("Incorrect Password",window.location.href="diary.html")
+    }
 })
 emailinp.addEventListener("focus",(e)=>{
     errormsg.style.display="none"
@@ -35,4 +43,3 @@ emailinp.addEventListener("focus",(e)=>{
 passinp.addEventListener("focus",(e)=>{
     errormsg2.style.display="none"
 });
-
