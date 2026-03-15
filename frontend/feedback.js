6c79a5c03444cc6403a69db51e366d28108ba770
indhumathi-3373 <indhumathi93428@gmail.com>
2026-03-15 16:43:18 +0530

    field null after submission

--- frontend/feedback.js
+++ frontend/feedback.js
@@ -1,10 +1,12 @@
 const f_name=document.getElementById("f_name")
 const feedback=document.getElementById("f_feed")
-const submit=document.getElementById("f_sub")
+const submit=document.getElementById("f_sub");
 submit.addEventListener("click",(e)=>{
     if(f_name.value ==="" || feedback.value===""){
         alert("please fill all fields before submit 📌")
     }else{
         alert("Thanks for your feedback! 🥰")
+        f_name.value="";
+        feedback.value="";
     }
 })
